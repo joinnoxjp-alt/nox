e0a4153c7f74623cd0c9e1b08d78046232433ad7
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-03 16:17:31 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBA8__C8pboCIjcPEwLWQL71juZeMMBti4",
+  authDomain: "noxa-b8f1a.firebaseapp.com",
+  projectId: "noxa-b8f1a",
+  storageBucket: "noxa-b8f1a.firebasestorage.app",
+  messagingSenderId: "679949162566",
+  appId: "1:679949162566:web:94a613ef0392798c57d247",
+  measurementId: "G-FBJGMJ63BH"
+};
