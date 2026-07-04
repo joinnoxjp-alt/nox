7a15a5cd3b4c6bbdd82c93bd28ab272fad0d2fe9
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-04 11:24:56 +0900

    Create firebase.js

---
+++ pages/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVUp3GMTtztLmac0e1XFCPYCPsapSL8QI",
+  authDomain: "noxapp-29171.firebaseapp.com",
+  projectId: "noxapp-29171",
+  storageBucket: "noxapp-29171.firebasestorage.app",
+  messagingSenderId: "783884878920",
+  appId: "1:783884878920:web:37a4c9f0c55b404a28c47d",
+  measurementId: "G-9DLCRXT98Y"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
