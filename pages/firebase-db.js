4e3e51c61df8e1043d2ac73eb678e7bfd5610603
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-11 03:13:03 +0900

    Update firebase-db.js

--- pages/firebase-db.js
+++ pages/firebase-db.js
@@ -11,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-9DLCRXT98Y"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
