da45509530da8da9746c76f91923f95eabba38c1
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-11 03:05:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -206,9 +206,8 @@ if (topJobs) {
       <div class="top-job-card">
 
         <img
-src="${job.imageUrl || job.image || 'images/line_oa_chat_260708_192846.jpeg'}"
-alt="${job.storeName || job.name || '求人画像'}"
-onerror="this.src='images/line_oa_chat_260708_192846.jpeg';"
+  src="images/line_oa_chat_260708_192846.jpeg"
+  alt="${job.storeName || job.name || '求人画像'}"
 />
 />
 
