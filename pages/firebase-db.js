005302960e844568657507c7861f6842ba88d66d
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-17 05:02:01 +0900

    Update firebase-db.js

--- pages/firebase-db.js
+++ pages/firebase-db.js
@@ -1,5 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVUp3GMTtztLmac0e1XFCPYCPsapSL8QI",
@@ -10,6 +19,9 @@ const firebaseConfig = {
   appId: "1:783884878920:web:37a4c9f0c55b404a28c47d",
   measurementId: "G-9DLCRXT98Y"
 };
+
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
