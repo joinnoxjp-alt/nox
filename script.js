7dceaaaf23ab37dd3bd93f6a03876cbeb3f7060e
joinnoxjp-alt <joinnoxjp@gmail.com>
2026-07-19 21:18:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // NOX script.js
 
-import { db, auth } from "./pages/firebase-db.js";
+import { db } from "./pages/firebase-db.js";
+import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
   collection,
